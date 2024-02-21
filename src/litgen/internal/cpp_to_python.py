e247b5fe4eaf55a25c67594b9638057f8f7f547f
davidlatwe <davidlatwe@gmail.com>
2024-02-21 23:27:18 +0800

    Workable nanobind generating code

--- src/litgen/internal/cpp_to_python.py
+++ src/litgen/internal/cpp_to_python.py
@@ -524,7 +524,7 @@ def standard_type_replacements() -> RegexReplacementList:
     \bvoid\s*\* -> Any
     \bvoid\b -> None
 
-    \bpy::array\b -> np.ndarray
+    \bpy::ndarray<(.*)> -> np.ndarray
 
     \bconst\b -> REMOVE
     \bmutable\b -> REMOVE
